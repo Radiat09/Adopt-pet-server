eb6af738b256cf76b5d326b6046f1afaeff93e2d
Radiat09 <rzsianboyridoy10@gmail.com>
2023-11-25 00:27:31 +0600

    get all pet data basic route and get category basic route complete

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 9000;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgalepw.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const petCollection = client.db("adoptPet").collection("pets");
+    const categoryCollection = client.db("adoptPet").collection("categories");
+    // Get all pet data
+    app.get("/api/v1/pets", async (req, res) => {
+      const result = await petCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get all the categories
+    app.get("/api/v1/categories", async (req, res) => {
+      const result = await categoryCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Adopt Pet is running");
